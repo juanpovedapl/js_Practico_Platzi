f81bea7953b088d2f8d760b4eb33c1d28ed2e906
juanpovedapl <juanpablopoveda93@gmail.com>
2021-12-24 21:13:39 -0500

    Termina el taller 2 , y se agrafga un reto en el que se debe hacer un descuento segun el bonno

--- descuentos.js
+++ descuentos.js
@@ -12,3 +12,30 @@ function calcularNuevoPrecio(){
     const rPrecio = document.getElementById("redultadoPrecio");
     rPrecio.innerText = "El precio con descuento  es " + nPrecio;
 }   
+
+function nuevoPrecioxCupon(){
+    const coupons =[
+        "Cumple",
+        "ClienteFiel",
+        "Gold"
+    ];
+    const couponValue = document.getElementById("InputCupon").value;
+    
+    let descuentoCupon = 200;
+    switch(couponValue){
+        case coupons[0]:
+            descuentoCupon = 15;
+        break;
+        case coupons[1]:
+            descuentoCupon=30;
+        break;
+        case coupons[2]:
+            descuentoCupon=25;
+        break;
+    }
+    const precio = Number(document.getElementById("InputCosto").value);
+    const nPrecio = nuevoPrecio(precio,descuentoCupon);
+    const precioCupon = document.getElementById("redultadoCupon");
+    precioCupon.innerText = "El descuento apllicado fue " + couponValue + " y el precio con descuento es: $" + nPrecio;
+
+}
